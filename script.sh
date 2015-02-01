db3f034f973030a936b39191d079369fa900ee88
thisiswei <99plus2@gmail.com>
2015-02-01 13:35:17 -0500

    steps in bashscript

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# read all the logs in reverse order
+
+
+# parse the log hashs
+
+
+# use git show and concat all the result in to one file
+
+
+# cd $(repo)
+# git log --reverse  --oneline > /tmp/gitlogs.txt
+# cat /tmp/gitlogs.txt | cut -d " " -f 1 > /tmp/commithashs.txt
+# cat /tmp/commithashs.txt | xargs git show > /tmp/$(repo)_commits.txt
